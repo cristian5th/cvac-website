588099f5a4163992c74a9cb40e2c2f632385c4bd
cristian5th <cralvarez@me.com>
2024-02-12 20:08:01 +0100

    Remove sample code

--- wp-content/themes/twentytwentyfour-child/functions.php
+++ wp-content/themes/twentytwentyfour-child/functions.php
@@ -4,17 +4,3 @@ function my_favicon_link() {
     echo '<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />' . "\n";
 }
 add_action( 'wp_head', 'my_favicon_link' );
-
-
-/**
- * override registrations form fields
- * @parma $fields
- * @return $fields
- **/
-function custom_function($fields) {
-    //customize $fields
-    $fields['food']['value'] = 'vegetables';
-
-return $fields;
-}
-add_filter( 'event_registration_form_fields', 'custom_function', 10, 1 );
