da0d08c7c33c27fd80cc012923d10cf60f31d453
cristian5th <cralvarez@me.com>
2024-05-12 20:21:12 +0200

    WP WooCommerce Enabling Full Template Support

--- wp-content/themes/cvac/functions.php
+++ wp-content/themes/cvac/functions.php
@@ -3,10 +3,10 @@
 add_action( 'wp_enqueue_scripts', 'cvac_enqueue_styles' );
 
 function cvac_enqueue_styles() {
-	wp_enqueue_style( 
-		'cvac-style', 
-		get_stylesheet_uri()
-	);
+    wp_enqueue_style( 
+        'cvac-style', 
+        get_stylesheet_uri()
+    );
 }
 
 // Add favicon to the site pages
@@ -14,6 +14,12 @@ function my_favicon_link() {
     echo '<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />' . "\n";
 }
 add_action( 'wp_head', 'my_favicon_link' );
+
+// WP WooCommerce Enabling Full Template Support
+function cvac_add_woocommerce_support() {
+    add_theme_support( 'woocommerce' );
+}
+add_action( 'after_setup_theme', 'cvac_add_woocommerce_support' );
 
 // WP Event Manager Enabling Full Template Support
 add_theme_support( 'event-manager-templates' );
