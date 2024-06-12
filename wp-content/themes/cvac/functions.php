7b71960229c0f06713d4e6fb672c5f42398935a3
cristian5th <cralvarez@me.com>
2024-06-12 21:58:06 +0200

    WP WooCommerce add vacation notice at Shop Loop and Single Product pages

--- wp-content/themes/cvac/functions.php
+++ wp-content/themes/cvac/functions.php
@@ -45,3 +45,27 @@ function my_redirect_non_logged_in_users() {
     }
 }
 add_action( 'template_redirect', 'my_redirect_non_logged_in_users' );
+
+// WP WooCommerce add vacation notice at Shop Loop and Single Product pages
+function my_woo_store_vacation_notice() {
+    $vacation_mode = woo_store_vacation()->service( 'options' )->get( 'vacation_mode', 'no' );
+    if ($vacation_mode == 'yes') {
+        $notice = woo_store_vacation()->service( 'options' )->get( 'vacation_notice' );
+        $end_date = woo_store_vacation()->service( 'options' )->get( 'end_date' );
+        $end_date_datetime = date_create( $end_date );
+        $end_date_format = date_format( $end_date_datetime, "d/m/Y" );
+        
+        // Bail early, if the notice is empty.
+        if ( empty( $notice ) && function_exists( 'wc_print_notice' ) ) {
+            $notice = "Le magasin est fermé jusqu'au $end_date_format. Nous vous remercions de votre patience et vous prions de nous excuser pour ce désagrément.";
+
+            printf( '<div id="%s">', esc_attr( woo_store_vacation()->get_slug() ) );
+            $message = wp_kses_post( nl2br( $notice ) );
+            wc_print_notice( $message, apply_filters( 'woo_store_vacation_notice_type', 'notice' ) );
+            echo '</div>';
+        }
+
+    }
+}
+add_action( 'woocommerce_before_shop_loop', 'my_woo_store_vacation_notice', 5 );
+add_action( 'woocommerce_before_single_product', 'my_woo_store_vacation_notice', 5 );
